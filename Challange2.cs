ef5368ee340a84dfe00375c96bd6672454c69406
leonTech254 <martinleontech23@gmail.com>
2023-11-28 12:54:06 +0300

    challange 2-inheritance

---
+++ Challange2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Inheritance:CHALLANGE 2
+Implement a base class "Shape" and derive classes like Circle, Rectangle, and Triangle. 
+Add methods to calculate the area and perimeter for each shape.
+*/
+namespace AdvanceC_Sharp
+{
+	public class Shape
+	{
+		private double width;
+		private double height;
+
+		public Shape(double width, double height)
+		{
+		Width = width;
+		Height = height;
+		}
+
+
+		public double Width
+		{
+			get { return width; }
+			set { width = value; }
+		}
+
+		public double Height
+		{
+			get { return height; }
+			set { height = value; }
+		}
+
+
+	}
+
+	public class Rectangle : Shape
+	{
+		public Rectangle(double width, double height) : base(width, height) { 
+		
+		
+		}
+
+		public void calculateRectangleArea()
+		{
+			/*Calculating the Area of the rectangle*/
+			double area = Width * Height;
+			Console.WriteLine($"The Area of the Rectangle is {area}");
+
+		}
+
+		public void calculateParameter()
+		{
+			double paremeter=2*(Width + Height);
+			Console.WriteLine($"The parater of the rectangle is {paremeter}");
+		}
+	}
+
+
+
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 using AdvanceC_Sharp;
 
 Console.WriteLine("Hello, World!");
-Mammal mammal= new Mammal("martin","house",2,"walking",true);
-mammal.printMamal();
+/*Mammal mammal= new Mammal("martin","house",2,"walking",true);
+mammal.printMamal();*/
+
+Rectangle rectangle = new Rectangle(20, 30);
+rectangle.calculateRectangleArea();
+rectangle.calculateParameter();
 
